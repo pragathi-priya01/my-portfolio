61d751a703b1460f0d1fd51e4fd596036ce57c5d
pragathi-priya01 <pragathipriya77@gmail.com>
2025-06-29 14:25:18 +0530

    Add files via upload

--- code.js
+++ code.js
@@ -1,26 +1,3 @@
 function toggleDarkMode(){
     document.body.classList.toggle('dark-mode');
 }
-const audio = document.getElementById("bg-music");
-const btn = document.getElementById("toggleMusic");
-let isPlaying = false;
-btn.addEventListener("touchstart",toggleMusic); 
-btn.addEventListener("click",toggleMusic);
-function toggleMusic() {
-    if (!isPlaying) {
-        audio.play().then(() => {
-            isPlaying = true;
-            btn.textContent = "Pause Background Music";
-        }).catch ((err)  => {
-        alert("playback failed.try tapping again.");
-        console.error("playback failed:",err);
-    }); 
-} else {
-    audio.pause();
-    isPlaying = false
-    btn.textContent ="Play Background Music";
-}
-}
-
-    
-    
