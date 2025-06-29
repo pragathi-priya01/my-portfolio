fad9f78e30f1f2e599b7554d307d155cb00c4d36
pragathi-priya01 <pragathipriya77@gmail.com>
2025-06-29 13:54:14 +0530

    Add files via upload

--- code.js
+++ code.js
@@ -1,12 +1,19 @@
 function toggleDarkMode(){
     document.body.classList.toggle('dark-mode');
 }
-function playMusic(){
-    const audio = document.getElementById("bg-music");
+const audio = document.getElementById("bg-music");
+const toggleBtn = document.getElementById("toggleMusic");
+toggleBtn.addEventListener("click",function() {
     if (audio.paused) {
-        audio.play();
-    }
-    else {
-        audio.pause();
-    }
+       audio.play().then(() => {
+        toggleBtn.textContent = "Pause Background Music";
+    }).catch((error) => {
+        alert("playback failed.try tapping again");
+        console.log(error);
+    });
+} else{
+    audio.pause();
+    toggleBtn.textContent ="Play Background Music";
 }
+});
+    
