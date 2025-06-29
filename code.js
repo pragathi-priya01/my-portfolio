4b7a900f98e5dd5ef28b5a6badad9dc2dd11bde8
pragathi-priya01 <pragathipriya77@gmail.com>
2025-06-29 13:07:21 +0530

    Add files via upload

--- code.js
+++ code.js
@@ -1,3 +1,12 @@
 function toggleDarkMode(){
     document.body.classList.toggle('dark-mode');
 }
+function playMusic(){
+    const audio = document.getElementById("bg-music");
+    if (audio.paused) {
+        audio.play();
+    }
+    else {
+        audio.pause();
+    }
+}
